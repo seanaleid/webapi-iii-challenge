b96a7308e16e9a0954522b67e5e5b4f95aab0f80
seanaleid <seanaleid@gmail.com>
2019-10-30 22:09:04 -0400

    Adds functional code for postRouter / GET

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -4,25 +4,50 @@ const router = express.Router();
 
 const postDb = require('./postDb');
 
+// 1
 router.get('/', (req, res) => {
 
+    postDb.get()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'There an error!'})
+        })
 });
 
-router.get('/:id', (req, res) => {
+// 2
+// router.get('/:id', validatePostId, (req, res) => {
 
-});
+// });
 
-router.delete('/:id', (req, res) => {
+// 3
+// router.delete('/:id', (req, res) => {
 
-});
+// });
 
-router.put('/:id', (req, res) => {
+// 4 
+// router.put('/:id', (req, res) => {
 
-});
+// });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
+
+    const {id} = req.params;
+
+    postDb.getById(id)
+    .then(postId => {
+        if(postId){
+            next();
+        } else {
+            res.status(400).json({ errorMessage: "Invalid user id." })
+        } 
+    })
+    .catch(err => {
+        console.log(res.status(500).json({error: 'there was an error validating the user id'})
+    )});
 
 };
 
